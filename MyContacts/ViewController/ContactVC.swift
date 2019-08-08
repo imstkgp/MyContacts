ced87a0484c6d4e7b6d711ded0ed6dd66d45e7e8
Santosh Tewari <Santosh.Tewari@makemytrip.com>
2019-08-08 20:32:44 +0530

    Initial setup

--- MyContacts/ViewController/ContactVC.swift
+++ MyContacts/ViewController/ContactVC.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ContactVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
